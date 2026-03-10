a00426a05ad637b98199099cb569078912505008
happykimsfamily <happykimsfamily@happykimsfamilyui-Macmini.local>
2026-03-10 18:10:28 +0900

    0310

--- sync-config.js
+++ sync-config.js
@@ -1,5 +1,5 @@
 window.SCHEDULE_SITE_SYNC_CONFIG = {
   remoteSyncEndpoint: "",
-  supabaseUrl: "",
-  supabaseAnonKey: ""
+  supabaseUrl: "https://pidfkrxsgffoqstogmli.supabase.co",
+  supabaseAnonKey: "sb_publishable_0JHUt_yXZx78FwoqO8XCDg_s319cLiW"
 };
